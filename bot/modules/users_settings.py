deffb404d0e749e4234a6b6155c6cfe6861e7e2c
clyfly <denwahgenshin@gmail.com>
2024-05-06 05:13:13 +0000

    bot: changes image on user settings
    Signed-off-by: clyfly <denwahgenshin@gmail.com>

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -128,7 +128,7 @@ async def update_user_settings(query):
     user_id = query.from_user.id
     tpath = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(tpath):
-        tpath = "https://graph.org/file/e903eff8c848aa2782928.png"
+        tpath = "https://graph.org/file/a6abb9cc4c69f92050477.png"
     await query.message.edit_media(
         media=InputMediaPhoto(media=tpath, caption=msg), reply_markup=button)
 
@@ -138,7 +138,7 @@ async def user_settings(_, message):
     user_id = message.from_user.id
     tpath = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(tpath):
-        tpath = "https://graph.org/file/e903eff8c848aa2782928.png"
+        tpath = "https://graph.org/file/a6abb9cc4c69f92050477.png"
     usetMsg = await message.reply_photo(tpath, caption=msg, reply_markup=button)
     await auto_delete_message(message, usetMsg)
 
